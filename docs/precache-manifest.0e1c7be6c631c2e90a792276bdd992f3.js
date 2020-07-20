0c620d7171f7b06a99db64894a0e224202312e8f
Mariano Quipildor <marianogq@gmail.com>
2020-07-20 17:03:52 -0300

    docs agregado

---
+++ docs/precache-manifest.0e1c7be6c631c2e90a792276bdd992f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a734c9793a358b8f8be8fd3ca06a8c77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b239e26ea94bf1ed295",
+    "url": "/static/css/main.ede17a45.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b239e26ea94bf1ed295",
+    "url": "/static/js/main.fcbb4485.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
